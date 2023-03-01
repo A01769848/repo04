246df40ac76315372506cec063dbddc87a990975
A01769848 <123998505+A01769848@users.noreply.github.com>
2023-03-01 15:50:34 -0600

    Add files via upload

---
+++ prog02_op_botones.js
@@ -0,0 +1,31 @@
+function suma(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x+y;
+	document.datos.opera.value="Suma";
+	document.datos.res.value=r;
+}
+
+function resta(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x-y;
+	document.datos.opera.value="Resta";
+	document.datos.res.value=r;
+}
+
+function mult(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x*y;
+	document.datos.opera.value="Multiplicación";
+	document.datos.res.value=r;
+}
+
+function div(){
+	x=parseInt(document.datos.v1.value);
+	y=parseInt(document.datos.v2.value);
+	r=x/y;
+	document.datos.opera.value="División";
+	document.datos.res.value=r;
+}
